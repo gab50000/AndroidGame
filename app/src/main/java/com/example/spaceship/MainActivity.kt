8a617747e2841ca935acd5c2e9c683bc618cb083
Gabriel Kabbe <gabriel.kabbe@mail.de>
2019-05-04 19:36:23 +0200

    First commit

---
+++ app/src/main/java/com/example/spaceship/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.spaceship
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
