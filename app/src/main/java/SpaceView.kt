c3f2539675dce688f49007cd35d48707e1393256
Gabriel Kabbe <gabriel.kabbe@mail.de>
2019-05-06 19:54:31 +0200

    Add paint, canvas and surfaceholder to SpaceView class

--- app/src/main/java/SpaceView.kt
+++ app/src/main/java/SpaceView.kt
@@ -1,12 +1,18 @@
 package com.example.spaceship
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.view.SurfaceHolder
 import android.view.SurfaceView
 
 class SpaceView(context: Context?) : SurfaceView(context), Runnable{
 
     private var playing = false
     private lateinit var gameThread: Thread
+    private lateinit var paint: Paint
+    private lateinit var canvas: Canvas
+    private lateinit var surfaceHolder: SurfaceHolder
 
     override fun run() {
         while (playing){
@@ -21,6 +27,7 @@ class SpaceView(context: Context?) : SurfaceView(context), Runnable{
     }
 
     fun draw(){
+        ourHolder = getHolder()
 
     }
 
